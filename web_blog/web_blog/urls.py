d1f657d492c7d19a1547b2b582e6f61649a719ac
jordan <jordan@payitmonthly.uk>
2019-08-30 15:08:21 +0100

    added custom auth app from password project

--- web_blog/web_blog/urls.py
+++ web_blog/web_blog/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('', include('blog.urls')),
     path('accounts/login/', views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('accounts/logout/', views.LogoutView.as_view(), name='logout', kwargs={'next_page': '/'}), # redirecting to '/' will take you back to the homepage or regex''
+    path('test/', include('apppass.urls'))
 ]
