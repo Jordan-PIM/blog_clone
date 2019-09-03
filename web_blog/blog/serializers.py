59bba4c36303a29615ce589c1dffc2c1b6c2abd4
jordan <jordan@payitmonthly.uk>
2019-09-03 15:38:42 +0100

    Altered the date output format in the serializer for the date created in the PostsSerializer.

--- web_blog/blog/serializers.py
+++ web_blog/blog/serializers.py
@@ -4,6 +4,8 @@ from .models import Post, Comment
 
 
 class PostSerializer(serializers.HyperlinkedModelSerializer):
+    created_date = serializers.DateTimeField(format='%d-%m-%y')
+    number = serializers.IntegerField(default=100)
     class Meta:
         model = Post
         fields = '__all__'
